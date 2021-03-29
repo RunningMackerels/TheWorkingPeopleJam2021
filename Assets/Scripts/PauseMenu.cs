ad20a2f5d180339ea0afb5a11744f9fe0da069be
bruno o <40021775+bo-esmad@users.noreply.github.com>
2021-03-29 23:14:27 +0100

    * Before Merging…

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(PauseMenu))]
+public class PauseMenu : MonoBehaviour
+{
+
+    public static PauseMenu Instance => _instance;
+    private static PauseMenu _instance = null;
+
+    private Canvas _canvas = default;
+    private bool _visible = false;
+    
+    #region Events
+    public static event Action ButtonResume;
+    public static event Action ButtonRestart;
+    public static event Action ButtonQuit;
+    #endregion
+
+    void VerifySingleton()
+    {
+        if (_instance != null && _instance != this)
+        {
+            DestroyImmediate(this);
+            return;
+        }
+
+        _instance = this;
+        _canvas = GetComponent<Canvas>();
+        _canvas.enabled = _visible;
+    }
+
+    private void Awake()
+    {
+        VerifySingleton();
+    }
+
+    public void Show()
+    {
+        Toggle();
+    }
+
+    private void Toggle()
+    {
+        _visible = !_visible;
+    }
+    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            OnButtonResume();
+        }
+        _canvas.enabled = _visible;
+    }
+
+    public bool IsVisible() => _visible;
+
+    public void OnButtonResume()
+    {
+        ButtonResume?.Invoke();
+        Toggle();
+    }
+
+    public void OnButtonRestart()
+    {
+        ButtonRestart?.Invoke();
+        Toggle();        
+    }
+    
+    public void OnButtonQuit()
+    {
+        ButtonQuit?.Invoke();
+        Toggle();
+    }
+}
+
---
+++ Assets/Scripts/TestPauseMenu.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPauseMenu : MonoBehaviour
+{
+
+    private void OnRestart()
+    {
+        Debug.Log("Master told me to restart...");
+    }
+    private void OnEnable()
+    {
+        PauseMenu.ButtonRestart += OnRestart;
+    }
+
+    private void OnDisable()
+    {
+        PauseMenu.ButtonRestart -= OnRestart;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!PauseMenu.Instance.IsVisible())
+            {
+                Debug.Log("Showing...");
+                PauseMenu.Instance.Show();
+            }
+        }
+    }
+}
