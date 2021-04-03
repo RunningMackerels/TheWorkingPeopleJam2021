a5fdbb4f30e0479cfa1e314f98941f95705279dc
Pedro <mendes.pmt@gmail.com>
2021-04-03 23:52:34 +0100

    First WebGL build (not working properly)

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,9 +1,5 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditorInternal;
 using UnityEngine;
-using UnityEngine.UI;
 
 [RequireComponent(typeof(PauseMenu))]
 public class PauseMenu : MonoBehaviour
